22fee8aef514d138b550ce4a2c392476d6d23707
Amritesh Nandan <100837734+Amriteshwork@users.noreply.github.com>
2023-04-06 13:18:22 +0530

    Update helper_finction.py

--- helper_finction.py
+++ helper_finction.py
@@ -19,3 +19,43 @@ def count_words_in_column(column):
     return df_word_counts
   
   ################################################################################################################
+
+## Scrap the data when the data is in JSON format in HTML page.
+
+import requests
+import json
+import csv
+
+url = 'https://newsdata.io/api/1/archive?apikey=pub_xxxxx&q=news'
+
+# get all data
+all_data = []
+
+while url:
+    response = requests.get(url)
+
+    try:
+        data = json.loads(response.content)
+    except json.JSONDecodeError:
+        print("Error: Invalid JSON")
+        break
+    else:
+        for item in data['results']:
+            all_data.append(item)
+
+        if data['nextPage']:
+            url = f"{url}&page={data['nextPage']}"
+            print(url)
+        else:
+            url = None
+
+if all_data:
+    keys = all_data[0].keys()
+
+    with open("data_given/content_1.csv", "w", newline='') as f:  # 9211    #	1598789
+        writer = csv.DictWriter(f, fieldnames=keys)
+        writer.writeheader()
+        for item in all_data:
+            writer.writerow(item)
+else:
+    print("Error: No data retrieved")
